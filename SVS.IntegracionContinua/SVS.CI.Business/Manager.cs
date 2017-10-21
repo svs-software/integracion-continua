dc7cb805cb30e2df4be26cec0d158a60f894f28d
Edson Castaneda <edsonbryan@gmail.com>
2017-10-21 00:26:47 -0500

    upload basic .net sample

---
+++ SVS.IntegracionContinua/SVS.CI.Business/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVS.CI.Business
+{
+    public class Manager
+    {
+        public void Covered()
+        {
+            Console.WriteLine("Running covered method");
+        }
+
+        public void Uncovered()
+        {
+            Console.WriteLine("Running uncovered method");
+        }
+    }
+}
---
+++ SVS.IntegracionContinua/SVS.CI.UnitTests/ManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SVS.CI.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVS.CI.UnitTests
+{
+    [TestFixture]
+    public class ManagerTests
+    {
+        [Test]
+        public void RunMethodTest()
+        {
+            var manager = new Manager();
+
+            manager.Covered();
+        }
+    }
+}
