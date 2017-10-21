2f0dab7d0f13d2f826545117ef76ee8698ca397d
Edson Castaneda <edsonbryan@gmail.com>
2017-10-21 01:02:56 -0500

    add new unit test

---
+++ SVS.IntegracionContinua/SVS.CI.Data/Database.cs
@@ -0,0 +1,17 @@
+﻿namespace SVS.CI.Data
+{
+    public class Database
+    {
+        public static bool TestConnection(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ SVS.IntegracionContinua/SVS.CI.UnitTests/DatabaseTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using SVS.CI.Data;
+
+namespace SVS.CI.UnitTests
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [Test]
+        public void TestConnection_KeyIsNotEmpty_ReturnsTrue()
+        {
+            var key = "connectionstring";
+
+            var result = Database.TestConnection(key);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
