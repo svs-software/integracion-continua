343ecf2f6477ca2cf81b4ff8f1c4f194efa9875a
Edson Castaneda <edsonbryan@gmail.com>
2017-10-27 19:31:47 -0500

    add dependency between projects

--- SVS.TDD/SVS.TDD.Negocio/ManagerPersona.cs
+++ SVS.TDD/SVS.TDD.Negocio/ManagerPersona.cs
@@ -6,19 +6,6 @@ namespace SVS.TDD.Negocio
 {
     public class ManagerPersona
     {
-        public static bool ValidarPersona(Persona persona)
-        {
-            // TODO: Validar NULL <persona>
-
-            if (string.IsNullOrEmpty(persona.Id))
-                throw new Exception("Id incorrecto");
-
-            if (string.IsNullOrEmpty(persona.Nombre))
-                throw new Exception("Nombre incorrecto");
-
-            return true;
-        }
-
         public static bool ValidarId(string id)
         {
             if (string.IsNullOrEmpty(id))
---
+++ SVS.TDD/SVS.TDD.UnitTests/DatabaseTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SVS.CI.Data;
+
+namespace SVS.TDD.UnitTests
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [Test]
+        public void PruebaTemporal()
+        {
+            var resultado = Database.TestConnection(null);
+
+            Assert.IsTrue(resultado);
+        }
+    }
+}
